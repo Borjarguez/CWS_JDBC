d8995e45de08028eaf03e36e5f38d5b46eb36780
Borja <uo258643@uniovi.es>
2019-10-22 22:26:37 +0200

    prueba

--- CWS/src/uo/ri/business/TransactionScripts/administrator/UpdateMechanic.java
+++ CWS/src/uo/ri/business/TransactionScripts/administrator/UpdateMechanic.java
@@ -1,15 +1,9 @@
 package uo.ri.business.TransactionScripts.administrator;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-import alb.util.jdbc.Jdbc;
 import uo.ri.business.dto.MechanicDto;
 
 public class UpdateMechanic {
-	private static String SQL = "update TMechanics " + "set name = ?, surname = ? " + "where id = ?";
+	private static String SQL_UPDATE_MECHANIC = "update TMechanics set name = ?, surname = ? where id = ?";
 	private MechanicDto m;
 	
 	
